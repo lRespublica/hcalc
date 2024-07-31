7a18ffb53ea74cd3c74d846478f62f8679054d65
Leonid Znamenok <respublica@altlinux.org>
2024-07-31 14:37:31 +0300

    Added function to run all parsers and get both value and type

--- HCalc/Useful/Grammar.hs
+++ HCalc/Useful/Grammar.hs
@@ -2,18 +2,20 @@ module HCalc.Useful.Grammar where
 import Data.Char
 import qualified Data.Map as Map
 
-availableMathSymbols = "+-*/"
+data TokenTypes = NUM | OPERATOR | FUNCTION | L_BRACKET | R_BRACKET
+                deriving Show
 
-operations = Map.fromList [('+', 5), ('-', 5), ('*', 4), ('*', 4)]
+operatorsList = [('+', 5), ('-', 5), ('*', 4), ('*', 4)]
+operators = Map.fromList operatorsList
 
 functionsList = [("abs", 2), ("sqrt", 3)]
 availableFunctions = Map.fromList functionsList
 
-isAvailableMathSymbol :: Char -> Bool
-isAvailableMathSymbol c = c `elem` availableMathSymbols
+isAvailableOperator :: Char -> Bool
+isAvailableOperator c = Map.member c operators
 
 isBracket :: Char -> Bool
 isBracket c = c `elem` "()"
 
 isAvailableSymbol :: Char -> Bool
-isAvailableSymbol sym = or ([isDigit, isSpace, isBracket, isAvailableMathSymbol] <*> pure sym)
+isAvailableSymbol sym = or ([isDigit, isSpace, isBracket, isAvailableOperator] <*> pure sym)
--- HCalc/Useful/Parsers.hs
+++ HCalc/Useful/Parsers.hs
@@ -7,26 +7,35 @@ import Data.List
 import Data.Maybe
 import Control.Applicative
 
-anySymb = satisfy (const True)
-digit = satisfy isDigit
 char x = satisfy (==x)
-space = satisfy isSpace
-symbol = satisfy isAvailableMathSymbol
-bracket = satisfy isBracket
-lBracket = char '('
-rBracket = char ')'
 
 string :: String -> Parser Char String
 string = foldr (\x y -> (:) <$> char x <*> y) (pure "")
+
+anySymb = satisfy (const True)
+digit = satisfy isDigit
+space = satisfy isSpace
+operator = satisfy isAvailableOperator
+bracket = satisfy isBracket
+lBracket = string "("
+rBracket = string ")"
 
 num = some digit
 
 allBrackets = (:) <$> bracket <*> allBrackets <|> anySymb *> allBrackets <|> pure []
 
 allFunctions = [ string (fst x) | x <- functionsList]
+allOperators = [ string [fst x] | x <- operatorsList]
 
-parseFunc :: String -> Maybe (String, String)
-parseFunc str = fromMaybe Nothing findFunc
+runParsersList parsers val = fromMaybe Nothing findFunc
     where
-    parsersResult = runParser <$> allFunctions <*> pure str
+    parsersResult = runParser <$> parsers <*> pure val
     findFunc = find (/= Nothing) parsersResult
+
+simpleParsers = [(NUM, num), (L_BRACKET, lBracket), (R_BRACKET, rBracket)]
+complexParsers = [(OPERATOR, allOperators), (FUNCTION, allFunctions)]
+
+runSimpleParsers = fmap runParser <$> simpleParsers
+runComplexParsers = fmap runParsersList <$> complexParsers
+
+runAllParsers = runSimpleParsers ++ runComplexParsers
