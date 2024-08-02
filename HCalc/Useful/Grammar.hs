03b74c77beab40005e356280408d49c58e4f8816
Leonid Znamenok <respublica@altlinux.org>
2024-08-02 14:41:38 +0300

    Added function to operators Map

--- HCalc/Useful/Grammar.hs
+++ HCalc/Useful/Grammar.hs
@@ -1,7 +1,7 @@
 module HCalc.Useful.Grammar where
 import Data.Char
 import qualified Data.Map as Map
-import Data.Maybe (fromJust)
+import Data.Maybe
 import HCalc.Utils.List
 
 data TokenTypes = NUM | OPERATOR | FUNCTION | L_BRACKET | R_BRACKET | COMMA
@@ -13,14 +13,33 @@ instance Semigroup TokenTypes where
 instance Monoid TokenTypes where
     mempty = NUM
 
-operatorsList = [("+", 4), ("-", 4), ("*", 5), ("*", 5)]
+usePlus :: Double -> Double -> Double
+usePlus a b = a + b
+useMinus :: Double -> Double -> Double
+useMinus a b = a - b
+useProd :: Double -> Double -> Double
+useProd a b = a * b
+useDiv :: Double -> Double -> Double
+useDiv a b = a / b
+
+operatorsList = [("+", (4, usePlus)), ("-", (4, useMinus)), ("*", (5, useProd)), ("/", (5, useDiv))]
 availableOperators = Map.fromList operatorsList
 
 compareOperators op1 op2 = case getPriorities of
                                 Just [pr1, pr2] -> Just (compare pr1 pr2)
                                 Nothing -> Nothing
     where
-    getPriorities = sequenceA (Map.lookup <$> [op1, op2] <*> pure availableOperators)
+    getPriorities = fmap fst <$> sequenceA (Map.lookup <$> [op1, op2] <*> pure availableOperators)
+
+useOperator :: String -> Double -> Double -> Maybe Double
+useOperator op varX varY | isNothing operation  = Nothing
+                         | otherwise = operation <*> pure varX <*> pure varY
+    where
+    operation = snd <$> Map.lookup op availableOperators
+
+hasHigherPriority op1 op2 = comparision == Just LT || comparision == Just EQ
+    where
+    comparision = compareOperators op1 op2
 
 functionsList = [("abs", 7), ("sqrt", 6)]
 availableFunctions = Map.fromList functionsList
