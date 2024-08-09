d4452efa6417358091d71542db2ccec433f6d06d
Leonid Znamenok <respublica@altlinux.org>
2024-08-09 16:19:54 +0300

    Added HFunctions module, which describes actions with HNumbers

---
+++ HCalc/Utils/HFunctions.hs
@@ -0,0 +1,30 @@
+module HCalc.Utils.HFunctions where
+
+import HCalc.Utils.HNumbers
+
+class (Eq a, Show a) => HFunc a where
+    getStrs :: a -> [String]
+    getPriority :: a -> Int
+    getType :: a -> HFunType
+    execFunc :: a -> [HNum] -> Maybe HNum
+
+data Operators = PLUS | MINUS | MUL | DIV deriving (Eq, Show, Enum, Bounded)
+
+instance HFunc Operators where
+    getStrs PLUS = ["+"]
+    getStrs MINUS = ["-"]
+    getStrs MUL = ["*"]
+    getStrs DIV = ["/"]
+
+    getPriority PLUS = 4
+    getPriority MINUS = 4
+    getPriority MUL = 5
+    getPriority DIV = 5
+
+    getType _ = INFIX
+
+    execFunc PLUS [a, b] = Just $ a + b
+    execFunc MINUS [a, b] = Just $ a - b
+    execFunc MUL [a, b] = Just $ a * b
+    execFunc DIV [a, b] = Just $ a / b
+    execFunc _ _ = Nothing
