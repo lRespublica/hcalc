492384a97bdc7501ef1621440b99b416118907a3
Leonid Znamenok <respublica@altlinux.org>
2024-08-11 00:08:10 +0300

    Rewrited Useful.Parsers module with new implementation of TokenTypes

--- HCalc/Useful/Parsers.hs
+++ HCalc/Useful/Parsers.hs
@@ -1,11 +1,16 @@
 module HCalc.Useful.Parsers where
 
-import HCalc.Utils.Parser
-import HCalc.Useful.Grammar
 import Data.Char
 import Data.List
 import Data.Maybe
+
 import Control.Applicative
+import Control.Monad
+
+import HCalc.Utils.TokenType
+import HCalc.Utils.HNumbers
+import HCalc.Utils.Parser
+import HCalc.Useful.Grammar
 
 char x = satisfy (==x)
 
@@ -24,25 +29,52 @@ comma = string ","
 num :: Parser Char String
 num = (++) <$> some digit <*> ((:) <$> char '.' <*> some digit) <|> some digit
 
+-- Parser that gets all brackets from expression
 allBrackets = (:) <$> bracket <*> allBrackets <|> anySymb *> allBrackets <|> pure []
 
-allFunctions = [ string (fst x) | x <- functionsList]
-allOperators = [ string (fst x) | x <- operatorsList]
+grammarTokens :: [(TokenType, Parser Char String)]
+grammarTokens = [(L_BRACKET, lBracket), (R_BRACKET, rBracket), (COMMA, comma)]
 
-runParsersList parsers val = fromMaybe Nothing findFunc
+numberTokens :: [(TokenType, Parser Char String)]
+numberTokens = [(NUM hNan, num)]
+
+operatorsTokens :: [(TokenType, Parser Char String)]
+operatorsTokens = funcStringToParsable <$> operatorStringsList
     where
-    parsersResult = runParser <$> parsers <*> pure val
-    findFunc = find (/= Nothing) parsersResult
+    funcStringToParsable (strVal, func) = (FUNCTION func, string strVal)
 
-simpleParsers = [(NUM, num), (L_BRACKET, lBracket), (R_BRACKET, rBracket), (COMMA, comma)]
-complexParsers = [(OPERATOR, allOperators), (FUNCTION, allFunctions)]
+type StringAfterParsing = String
+type StringToParse = String
 
-runSimpleParsers :: [(TokenTypes, [Char] -> Maybe ([Char], [Char]))]
-runSimpleParsers = fmap runParser <$> simpleParsers
-runComplexParsers = fmap runParsersList <$> complexParsers
-runAllParsers = runSimpleParsers ++ runComplexParsers
+runTokenParser :: (TokenType, Parser Char String) -> StringToParse -> Maybe (TokenType, StringAfterParsing)
+runTokenParser _ [] = Just (END, [])
 
-parseToken str = fromMaybe Nothing findToken
-    where
-    parsersResult = sequenceA <$> (sequenceA <$> runAllParsers <*> pure str)
-    findToken = find (/= Nothing) parsersResult
+-- A separate implementation is needed for numbers, because the TokenType
+-- constructor does not contain information about the number.
+-- By default it contains NaN.
+runTokenParser (NUM _, parser) str = do
+                                   (remaining, readed) <- runParser parser str
+                                   let readedAsHNumToken = NUM $ readHNum readed
+                                   return (readedAsHNumToken, remaining)
+
+-- We do not need a separate implementation for functions,
+-- because the pair (TokenType FUNCTION f, String Representation) already
+-- contains information about function f.
+-- Therefore, we calmly return the token with meaningful information.
+runTokenParser (token, parser) str = do
+                                     (remaining, _) <- runParser parser str
+                                     return (token, remaining)
+
+allParsableTokens = join [grammarTokens, numberTokens, operatorsTokens]
+
+-- Running all parsers with folding
+parseToken :: String -> Maybe (TokenType, StringAfterParsing)
+parseToken str = foldr (\x y -> y <|> runTokenParser x str) Nothing allParsableTokens
+
+parseStringForTokens :: String -> Maybe [TokenType]
+parseStringForTokens str = do
+                            (newToken, nextStr) <- parseToken str
+                            case newToken of
+                                END -> return [END]
+                                otherToken -> do nextTokens <- parseStringForTokens nextStr
+                                                 return (newToken : nextTokens)
